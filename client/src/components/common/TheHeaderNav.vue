d38b8df6d0aa170c6c9ec7e35630fb476764afcc
youngho <youngho.dev@gmail.com>
2024-05-13 16:49:13 +0900

    Chore: vue.js 프로젝트 생성

---
+++ client/src/components/common/TheHeaderNav.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-4xl mx-64 my-10 font-mono text-green-500 font-bold hover:text-green-800 w-64">TheHeaderNav</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-3xl mx-64 my-10 font-mono text-green-500 font-bold hover:text-green-800 w-64">HomeView</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
