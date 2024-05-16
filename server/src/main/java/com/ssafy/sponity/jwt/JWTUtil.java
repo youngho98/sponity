c39d99f8e9b3e6deb3a19092b7d386b16b5c0199
kevinlee94 <kybin19@naver.com>
2024-05-17 00:05:16 +0900

    Refactor: JWT 관련 코드 리팩토링 및 주석 보강

--- server/src/main/java/com/ssafy/sponity/jwt/JWTUtil.java
+++ server/src/main/java/com/ssafy/sponity/jwt/JWTUtil.java
@@ -12,22 +12,20 @@ import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Jwts;
 
 /*
- * JWT의 발급과 검증을 담당하는 클래스입니다.
+ * JWT(Json Web Token)의 발급과 검증을 담당하는 클래스입니다.
  */
 @Component
 public class JWTUtil {
-
-    private SecretKey secretKey;
-    
     
     // application.properties에 저장된 암호화 키를 기반으로 토큰을 암호화
-    public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
-        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+	private SecretKey secretKey;
+    public JWTUtil(@Value("${spring.jwt.secret}") String secret) {
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
 
     
     // JWT 확인
-    // - 우리 서버에서 발급된 토큰이 맞는지 검증한 후, 맞다면 사용자 정보 반환
+    // - 비밀키로 토큰 검증, 클레임 파싱, 클레임에서 속성 값 추출 등의 순서를 거칩니다.
     public String getUserId(String token) {
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("userId", String.class);
     }
@@ -40,6 +38,7 @@ public class JWTUtil {
 
     
     // JWT 발급
+    // - 토큰의 payload 부분에 claim을 저장하고, 발급 및 만료 시간을 설정합니다.
     public String createJwt(String userId, String nickname, String role, Long expiredMs) {
         return Jwts.builder()
                 .claim("userId", userId)
