c31dc38bb5e53b4af28e60ed01f3c6991433dcff
youngho <youngho.dev@gmail.com>
2024-05-16 10:51:38 +0900

    Feat: 아이디 찾기, 비밀번호 재설정 페이지 구현

--- client/src/components/user/ResetPwForm.vue
+++ client/src/components/user/ResetPwForm.vue
@@ -2,7 +2,7 @@
 
   <div class="flex flex-col w-full max-w-md px-8 py-8 bg-white rounded-lg shadow shadow-gray-500 mx-auto my-40">
     <div class="self-center mb-6 text-xl font-light text-gray-500 sm:text-2xl dark:text-white">
-      Login To Your Account
+      Reset Your Password
     </div>
     <div class="mt-8">
       <form action="#" autoComplete="off">
@@ -22,7 +22,7 @@
                 </g>
               </svg>
             </span>
-            <input type="text" v-model="loginInfo.userId"
+            <input type="text" v-model="userInfo.userId"
               class=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent"
               placeholder="Your ID" />
           </div>
@@ -34,25 +34,25 @@
               <svg width="15" height="15" fill="currentColor" viewBox="0 0 1792 1792"
                 xmlns="http://www.w3.org/2000/svg">
                 <path
-                  d="M1376 768q40 0 68 28t28 68v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-320q0-185 131.5-316.5t316.5-131.5 316.5 131.5 131.5 316.5q0 26-19 45t-45 19h-64q-26 0-45-19t-19-45q0-106-75-181t-181-75-181 75-75 181v320h736z">
+                  d="M1792 710v794q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-794q44 49 101 87 362 246 497 345 57 42 92.5 65.5t94.5 48 110 24.5h2q51 0 110-24.5t94.5-48 92.5-65.5q170-123 498-345 57-39 100-87zm0-294q0 79-49 151t-122 123q-376 261-468 325-10 7-42.5 30.5t-54 38-52 32.5-57.5 27-50 9h-2q-23 0-50-9t-57.5-27-52-32.5-54-38-42.5-30.5q-91-64-262-182.5t-205-142.5q-62-42-117-115.5t-55-136.5q0-78 41.5-130t118.5-52h1472q65 0 112.5 47t47.5 113z">
                 </path>
               </svg>
             </span>
-            <input type="password" v-model="loginInfo.password"
+            <input type="text" v-model="userInfo.email"
               class=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent"
-              placeholder="Your password" />
+              placeholder="Your email address" />
           </div>
         </div>
         <div class="flex items-center mb-6 -mt-4">
           <div class="flex ml-auto">
             <RouterLink :to="{ name: 'findIdForm' }"
               class="inline-flex text-sm font-thin text-gray-500 hover:text-gray-700">
-              Forgot Your ID/Password?
+              Forgot Your ID?
             </RouterLink>
           </div>
         </div>
         <div class="flex w-full">
-          <input type="button" value="Login" @click="login"
+          <input type="button" value="Submit" @click="resetPw"
             class="py-2 px-4  bg-green-600 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg " />
         </div>
       </form>
@@ -72,17 +72,16 @@
 <script setup>
 import { useUserStore } from '@/stores/user';
 import { ref } from 'vue';
-import { RouterLink } from 'vue-router';
 
-const loginInfo = ref({
+const userInfo = ref({
   userId: '',
-  password: ''
+  email: ''
 });
 
 const store = useUserStore();
 
-const login = function () {
-  store.login(loginInfo);
+const resetPw = function () {
+  store.resetPw(userInfo);
 }
 </script>
 
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,6 +4,8 @@ import HomeView from '@/views/HomeView.vue';
 
 import RegisterForm from '@/components/user/RegisterForm.vue';
 import LoginForm from '@/components/user/LoginForm.vue';
+import FindIdForm from '@/components/user/FindIdForm.vue';
+import ResetPwForm from '@/components/user/ResetPwForm.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +24,16 @@ const router = createRouter({
       path: '/login',
       name: 'loginForm',
       component: LoginForm,
+    },
+    {
+      path: '/find-id',
+      name: 'findIdForm',
+      component: FindIdForm,
+    },
+    {
+      path: '/reset-pw',
+      name: 'resetPwForm',
+      component: ResetPwForm,
     },
   ]
 })
