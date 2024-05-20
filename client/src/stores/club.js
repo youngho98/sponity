590f66b91274c075dc850e3eb804d61d2f438c2f
youngho <youngho.dev@gmail.com>
2024-05-20 14:56:31 +0900

    Feat: 클럽 좋아요 기능 구현

--- client/src/stores/club.js
+++ client/src/stores/club.js
@@ -2,10 +2,14 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import axios from 'axios';
 import router from '@/router';
+import { useUserStore } from './user';
+import { useRoute } from 'vue-router';
 
 const URL = "http://localhost:8080/club";
 
 export const useClubStore = defineStore('club', () => {
+
+  const route = useRoute();
 
   const clubList = ref([]);
 
@@ -34,10 +38,42 @@ export const useClubStore = defineStore('club', () => {
     })
       .then((response) => {
         clubInfo.value = response.data;
+        useUserStore().loginUser.userStatus = response.data.userStatus;
+        useUserStore().loginUser.isLike = response.data.isLike;
       })
       .catch(() => {
         alert("클럽 조회에 실패했습니다.");
       })
+  }
+
+  const like = function() {
+    axios.get(`${URL}/${route.params.clubId}/like`, {
+      headers: {
+        Authorization: sessionStorage.getItem('access-token')
+      }
+    })
+    .then(() => {
+      useUserStore().loginUser.isLike = 1;
+      router.go(0);
+    })
+    .catch(() => {
+      alert("server error");
+    })
+  }
+
+  const unlike = function() {
+    axios.delete(`${URL}/${route.params.clubId}/like`, {
+      headers: {
+        Authorization: sessionStorage.getItem('access-token')
+      }
+    })
+    .then(() => {
+      useUserStore().loginUser.isLike = 0;
+      router.go(0);
+    })
+    .catch(() => {
+      alert("server error");
+    })
   }
 
   return {
@@ -45,6 +81,8 @@ export const useClubStore = defineStore('club', () => {
     clubInfo,
     search,
     getClubInfo,
+    like,
+    unlike,
   }
 }, {
   persist: {
