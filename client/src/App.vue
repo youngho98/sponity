77fe4f8d8c036a27a14712e8d3bda6a75f995b34
youngho <youngho.dev@gmail.com>
2024-05-22 15:38:28 +0900

    Feat: Footer 구현

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import TheHeaderNav from '@/components/common/TheHeaderNav.vue'
+import TheFooterNav from '@/components/common/TheFooterNav.vue'
 </script>
 
 <template>
   <TheHeaderNav />
   <RouterView />
+  <TheFooterNav />
 </template>
 
 <style scoped>
