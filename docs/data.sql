7f6c66280065023ec09cd21476ce46eb7ec5505e
youngho <youngho.dev@gmail.com>
2024-05-23 15:29:25 +0900

    Chore: 샘플 데이터 추가

---
+++ docs/data.sql
@@ -0,0 +1,161 @@
+-- 유저 등록
+-- 비밀번호: qwer12!@
+insert into user(user_id, password, user_name, nickname, email, wide_area, detail_area)
+values ('youngho98', '$2a$10$0uglUFipGXz8N.yr.DRfw.6C3nm54cVQZR2Wvsyil7AvBB1FstaUG', '노영호', '영호', 'youngho.dev@gmail.com', '대전', '유성구');
+insert into user(user_id, password, user_name, nickname, email, wide_area, detail_area, profile_img)
+values ('kevinlee94', '$2a$10$0uglUFipGXz8N.yr.DRfw.6C3nm54cVQZR2Wvsyil7AvBB1FstaUG', '이규빈', '규빈', 'kyubin.dev@gmail.com', '대전', '유성구', 'https://sponity.s3.ap-northeast-2.amazonaws.com/profile-picture/1716431170494_human.jpeg');
+insert into user(user_id, password, user_name, nickname, email, wide_area, detail_area, profile_img)
+values ('qwer1', '$2a$10$0uglUFipGXz8N.yr.DRfw.6C3nm54cVQZR2Wvsyil7AvBB1FstaUG', '김싸피', '테니스', 'helloworld1@gmail.com', '대전', '유성구', 'https://sponity.s3.ap-northeast-2.amazonaws.com/profile-picture/1716431427464_tennis.jpeg');
+insert into user(user_id, password, user_name, nickname, email, wide_area, detail_area, profile_img)
+values ('qwer2', '$2a$10$0uglUFipGXz8N.yr.DRfw.6C3nm54cVQZR2Wvsyil7AvBB1FstaUG', '김싸피', '축구공', 'helloworld2@gmail.com', '대전', '유성구', 'https://sponity.s3.ap-northeast-2.amazonaws.com/profile-picture/1716431326981_soccerBall.jpeg');
+insert into user(user_id, password, user_name, nickname, email, wide_area, detail_area, profile_img)
+values ('qwer3', '$2a$10$0uglUFipGXz8N.yr.DRfw.6C3nm54cVQZR2Wvsyil7AvBB1FstaUG', '김싸피', '토끼', 'helloworld3@gmail.com', '대전', '유성구', 'https://sponity.s3.ap-northeast-2.amazonaws.com/profile-picture/1716431299477_rabbit.jpeg');
+insert into user(user_id, password, user_name, nickname, email, wide_area, detail_area, profile_img)
+values ('qwer4', '$2a$10$0uglUFipGXz8N.yr.DRfw.6C3nm54cVQZR2Wvsyil7AvBB1FstaUG', '김싸피', '문어', 'helloworld4@gmail.com', '대전', '유성구', 'https://sponity.s3.ap-northeast-2.amazonaws.com/profile-picture/1716431232207_octopus.png');
+insert into user(user_id, password, user_name, nickname, email, wide_area, detail_area, profile_img)
+values ('qwer5', '$2a$10$0uglUFipGXz8N.yr.DRfw.6C3nm54cVQZR2Wvsyil7AvBB1FstaUG', '김싸피', '고양이', 'helloworld13@gmail.com', '대전', '유성구', 'https://sponity.s3.ap-northeast-2.amazonaws.com/profile-picture/1716430836873_cat.jpeg');
+insert into user(user_id, password, user_name, nickname, email, wide_area, detail_area, profile_img)
+values ('qwer6', '$2a$10$0uglUFipGXz8N.yr.DRfw.6C3nm54cVQZR2Wvsyil7AvBB1FstaUG', '김싸피', '선글이', 'helloworld6@gmail.com', '대전', '유성구', 'https://sponity.s3.ap-northeast-2.amazonaws.com/profile-picture/1716431397647_sunglasses.png');
+insert into user(user_id, password, user_name, nickname, email, wide_area, detail_area, profile_img)
+values ('qwer7', '$2a$10$0uglUFipGXz8N.yr.DRfw.6C3nm54cVQZR2Wvsyil7AvBB1FstaUG', '김싸피', '유니콘', 'helloworld7@gmail.com', '대전', '유성구', 'https://sponity.s3.ap-northeast-2.amazonaws.com/profile-picture/1716431461435_unicorn.png');
+insert into user(user_id, password, user_name, nickname, email, wide_area, detail_area, profile_img)
+values ('qwer8', '$2a$10$0uglUFipGXz8N.yr.DRfw.6C3nm54cVQZR2Wvsyil7AvBB1FstaUG', '김싸피', '물고기', 'helloworld8@gmail.com', '대전', '유성구', 'https://sponity.s3.ap-northeast-2.amazonaws.com/profile-picture/1716430887075_deathFish.png');
+insert into user(user_id, password, user_name, nickname, email, wide_area, detail_area, profile_img)
+values ('qwer9', '$2a$10$0uglUFipGXz8N.yr.DRfw.6C3nm54cVQZR2Wvsyil7AvBB1FstaUG', '김싸피', '병아리', 'helloworld9@gmail.com', '대전', '유성구', 'https://sponity.s3.ap-northeast-2.amazonaws.com/profile-picture/1716431200229_miniChicken.png');
+insert into user(user_id, password, user_name, nickname, email, wide_area, detail_area, profile_img)
+values ('qwer10', '$2a$10$0uglUFipGXz8N.yr.DRfw.6C3nm54cVQZR2Wvsyil7AvBB1FstaUG', '김싸피', '악마', 'helloworld10@gmail.com', '대전', '유성구', 'https://sponity.s3.ap-northeast-2.amazonaws.com/profile-picture/1716430934656_devil.png');
+insert into user(user_id, password, user_name, nickname, email, wide_area, detail_area, profile_img)
+values ('qwer11', '$2a$10$0uglUFipGXz8N.yr.DRfw.6C3nm54cVQZR2Wvsyil7AvBB1FstaUG', '김싸피', '하트', 'helloworld11@gmail.com', '대전', '유성구', 'https://sponity.s3.ap-northeast-2.amazonaws.com/profile-picture/1716431135136_heart.png');
+insert into user(user_id, password, user_name, nickname, email, wide_area, detail_area, profile_img)
+values ('qwer12', '$2a$10$0uglUFipGXz8N.yr.DRfw.6C3nm54cVQZR2Wvsyil7AvBB1FstaUG', '김싸피', '강아지', 'helloworld12@gmail.com', '대전', '유성구', 'https://sponity.s3.ap-northeast-2.amazonaws.com/profile-picture/1716431062340_dog.jpeg');
+insert into user(user_id, password, user_name, nickname, email, wide_area, detail_area, profile_img)
+values ('qwer13', '$2a$10$0uglUFipGXz8N.yr.DRfw.6C3nm54cVQZR2Wvsyil7AvBB1FstaUG', '김싸피', '돼지', 'helloworld5@gmail.com', '대전', '유성구', 'https://sponity.s3.ap-northeast-2.amazonaws.com/profile-picture/1716437571682_pig.png');
+
+-- 축구 클럽 등록(축구, 대전 유성구)
+insert into club(club_name, category, wide_area, detail_area, introduction, club_img)
+values ('프리미어', '축구 ⚽️', '대전', '유성구', '매주 토요일 아침 8시 한밭풋살장', 'https://sponity.s3.ap-northeast-2.amazonaws.com/club-picture/1716433406152_premiere.png');
+insert into member(user_id, club_id, leader)
+values ('youngho98', 1, 'Y');
+insert into club(club_name, category, wide_area, detail_area, introduction, club_img)
+values ('I LOVE FOOTBALL', '축구 ⚽️', '대전', '유성구', '축구를 사랑하는 사람들의 모임', 'https://sponity.s3.ap-northeast-2.amazonaws.com/club-picture/1716433599075_iLoveFootball.jpeg');
+insert into member(user_id, club_id, leader)
+values ('kevinlee94', 2, 'Y');
+insert into club(club_name, category, wide_area, detail_area, introduction, club_img)
+values ('사커러브', '축구 ⚽️', '대전', '유성구', '', 'https://sponity.s3.ap-northeast-2.amazonaws.com/club-picture/1716433685508_soccerlove.jpeg');
+insert into member(user_id, club_id, leader)
+values ('youngho98', 3, 'Y');
+insert into club(club_name, category, wide_area, detail_area, introduction, club_img)
+values ('호날두', '축구 ⚽️', '대전', '유성구', 'siuuuuuuuu!', 'https://sponity.s3.ap-northeast-2.amazonaws.com/club-picture/1716433795949_ronaldo.jpeg');
+insert into member(user_id, club_id, leader)
+values ('youngho98', 4, 'Y');
+insert into club(club_name, category, wide_area, detail_area, introduction, club_img)
+values ('메시', '축구 ⚽️', '대전', '유성구', 'Lionel Messi!!!', 'https://sponity.s3.ap-northeast-2.amazonaws.com/club-picture/1716434031026_messi.jpeg');
+insert into member(user_id, club_id, leader)
+values ('youngho98', 5, 'Y');
+
+-- 야구 클럽 등록(야구, 서울 송파구)
+insert into club(club_name, category, wide_area, detail_area, introduction, club_img)
+values ('메이저 야구클럽', '야구 ⚾️', '서울', '송파구', '함께 야구 해요!', 'https://sponity.s3.ap-northeast-2.amazonaws.com/club-picture/1716432043372_mlb.jpeg');
+insert into member(user_id, club_id, leader)
+values ('youngho98', 6, 'Y');
+insert into club(club_name, category, wide_area, detail_area, introduction, club_img)
+values ('두산 베어스', '야구 ⚾️', '서울', '송파구', '잠실 구장으로 집합!!!', 'https://sponity.s3.ap-northeast-2.amazonaws.com/club-picture/1716432173691_doosan.png');
+insert into member(user_id, club_id, leader)
+values ('youngho98', 7, 'Y');
+insert into club(club_name, category, wide_area, detail_area, introduction, club_img)
+values ('최강한화', '야구 ⚾️', '서울', '송파구', '이번엔 한화가 우승한다...', 'https://sponity.s3.ap-northeast-2.amazonaws.com/club-picture/1716432354765_hanhwa.png');
+insert into member(user_id, club_id, leader)
+values ('youngho98', 8, 'Y');
+
+-- 축구 클럽에 멤버들 가입 (1, 2번 클럽만)
+insert into member(user_id, club_id) values ('kevinlee94', 1);
+insert into member(user_id, club_id) values ('qwer1', 1);
+insert into member(user_id, club_id) values ('qwer2', 1);
+insert into member(user_id, club_id) values ('qwer3', 1);
+insert into member(user_id, club_id) values ('qwer4', 1);
+insert into member(user_id, club_id) values ('qwer5', 1);
+insert into member(user_id, club_id) values ('qwer6', 1);
+insert into member(user_id, club_id) values ('qwer7', 1);
+insert into member(user_id, club_id) values ('qwer8', 1);
+insert into member(user_id, club_id) values ('qwer9', 1);
+insert into member(user_id, club_id) values ('qwer10', 1);
+insert into member(user_id, club_id) values ('qwer11', 1);
+insert into member(user_id, club_id) values ('qwer12', 1);
+insert into member(user_id, club_id) values ('qwer13', 1);
+insert into member(user_id, club_id) values ('qwer1', 2);
+insert into member(user_id, club_id) values ('qwer2', 2);
+insert into member(user_id, club_id) values ('qwer3', 2);
+insert into member(user_id, club_id) values ('qwer5', 2);
+insert into member(user_id, club_id) values ('qwer7', 2);
+insert into member(user_id, club_id) values ('qwer9', 2);
+insert into member(user_id, club_id) values ('qwer11', 2);
+insert into member(user_id, club_id) values ('qwer13', 2);
+
+-- 축구클럽 좋아요 누르기 (1, 2번 클럽만)
+insert into club_like(user_id, club_id) values ('qwer1', 1);
+insert into club_like(user_id, club_id) values ('qwer2', 1);
+insert into club_like(user_id, club_id) values ('qwer3', 1);
+insert into club_like(user_id, club_id) values ('qwer4', 1);
+insert into club_like(user_id, club_id) values ('qwer5', 1);
+insert into club_like(user_id, club_id) values ('qwer6', 1);
+insert into club_like(user_id, club_id) values ('qwer7', 1);
+insert into club_like(user_id, club_id) values ('qwer8', 1);
+insert into club_like(user_id, club_id) values ('qwer9', 1);
+insert into club_like(user_id, club_id) values ('qwer10', 1);
+insert into club_like(user_id, club_id) values ('qwer1', 2);
+insert into club_like(user_id, club_id) values ('qwer3', 2);
+insert into club_like(user_id, club_id) values ('qwer5', 2);
+insert into club_like(user_id, club_id) values ('qwer7', 2);
+insert into club_like(user_id, club_id) values ('qwer9', 2);
+
+-- 1번 클럽 게시글
+insert into board(club_id, user_id, title, content, view_cnt, reg_date, img_1, img_2, img_3)
+values (1, 'youngho98', '이번주 토요일 풋살 경기 있습니다.', '참여 가능한 인원들 댓글 달아주세요!', 24, '2024-05-23 14:12:57',
+'https://sponity.s3.ap-northeast-2.amazonaws.com/board-picture/1716441177645_footsal1.jpeg', 
+'https://sponity.s3.ap-northeast-2.amazonaws.com/board-picture/1716441178116_footsal2.jpeg', 
+'https://sponity.s3.ap-northeast-2.amazonaws.com/board-picture/1716441178271_footsal3.jpeg');
+insert into board(club_id, user_id, title, content, view_cnt, reg_date, img_1, img_2, img_3)
+values (1,'kevinlee94', '내일 비온다고 하네요...', '오랜만에 풋살 하고 싶었는데 아쉬워요...', 14, '2024-05-24 08:45:36',
+'https://sponity.s3.ap-northeast-2.amazonaws.com/board-picture/1716441716506_rain1.jpeg', 
+'https://sponity.s3.ap-northeast-2.amazonaws.com/board-picture/1716441716869_rain2.jpeg', 
+'https://sponity.s3.ap-northeast-2.amazonaws.com/board-picture/1716441717024_rain3.jpeg');
+insert into board(club_id, user_id, title, content, view_cnt, reg_date, img_1)
+values (1,'qwer5', '오늘 풋살 너무 재밌었어요!', '담주에 또 봐요 여러분', 27, '2024-05-18 14:18:43',
+'https://sponity.s3.ap-northeast-2.amazonaws.com/board-picture/1716442675232_goal.jpeg');
+insert into board(club_id, user_id, title, content, view_cnt, reg_date)
+values (1,'qwer10', '가입인사드려요~', '오늘 가입했습니다~ 다들 잘 부탁드려요!', 13, '2024-05-19 20:19:53');
+insert into board(club_id, user_id, title, content, view_cnt, reg_date, img_1)
+values (1,'qwer12', '결국 이번에도 시티가 우승했네요', '아스날은 또 2등...', 19, '2024-05-20 03:21:09',
+'https://sponity.s3.ap-northeast-2.amazonaws.com/board-picture/1716443026062_citywin.jpeg');
+
+-- 2번 클럽 게시글
+insert into board(club_id, user_id, title, content, view_cnt, reg_date, img_1, img_2, img_3)
+values (2, 'qwer2', '챔스 결승 대진 나왔네요', '레알마드리드 vs 도르트문트', 29, '2024-05-09 08:37:24',
+'https://sponity.s3.ap-northeast-2.amazonaws.com/board-picture/1716443615053_championsLeague.jpeg', 
+'https://sponity.s3.ap-northeast-2.amazonaws.com/board-picture/1716443615377_realMadrid.jpeg', 
+'https://sponity.s3.ap-northeast-2.amazonaws.com/board-picture/1716443615527_dortmund.jpeg');
+insert into board(club_id, user_id, title, content, view_cnt, reg_date, img_1)
+values (2,'qwer3', '오랜만에 축구보고 왔어요', '재밌었는데 져서 아쉽네요..', 15, '2024-05-18 20:12:23',
+'https://sponity.s3.ap-northeast-2.amazonaws.com/board-picture/1716444400868_hanacitizen.jpeg');
+insert into board(club_id, user_id, title, content, view_cnt, reg_date, img_1)
+values (2,'kevinlee94', '클럽 만들었어요', '같이 축구 얘기도하고 만나서 축구도 해봐요!', 34, '2024-04-29 18:15:17',
+'https://sponity.s3.ap-northeast-2.amazonaws.com/board-picture/1716444528910_son.jpeg');
+
+-- 댓글
+select * from review;
+insert into review(board_id, user_id, content, reg_date)
+values (2, 'youngho98', '아쉽네요..ㅠㅠ', '2024-05-24 08:58:59');
+insert into review(board_id, user_id, content, reg_date)
+values (1, 'qwer7', '아쉽네요..ㅠㅠ', '2024-05-23 16:13:24');
+insert into review(board_id, user_id, content, reg_date)
+values (1, 'kevinlee94', '저도요!', '2024-05-23 17:01:53');
+insert into review(board_id, user_id, content, reg_date)
+values (1, 'youngho98', '다들 내일 뵈어요!', '2024-05-23 18:29:39');
+insert into review(board_id, user_id, content, reg_date)
+values (1, 'qwer10', '저도 참여하고 싶은데 어디로 가면 될까요?', '2024-05-23 19:11:19');
+insert into review(board_id, user_id, content, reg_date)
+values (1, 'qwer9', '한밭풋살장 오전 8시에요', '2024-05-23 19:18:49');
+insert into review(board_id, user_id, content, reg_date)
+values (6, 'qwer13', '결승은 언제인가요?', '2024-05-09 19:31:19');
+insert into review(board_id, user_id, content, reg_date)
+values (6, 'qwer2', '6월 2일 새벽 4시입니다', '2024-05-23 19:37:08');
