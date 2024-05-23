89172d0eca332c47af9ab2e9889f12a599152227
youngho <youngho.dev@gmail.com>
2024-05-23 20:40:01 +0900

    Fix: 이메일 불편접수 시 서버에서 운영자에게 이메일을 보내도록 수정

--- server/src/main/java/com/ssafy/sponity/controller/MailController.java
+++ server/src/main/java/com/ssafy/sponity/controller/MailController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +21,14 @@ public class MailController {
     private static final String FROM_ADDRESS = "sponity@naver.com";
     
     // 이메일 전송
+    @PostMapping
     public void claimMail(@RequestBody MailDTO mailDTO) {
     	SimpleMailMessage claimMail = new SimpleMailMessage();
     	
-    	claimMail.setFrom(FROM_ADDRESS);
-    	claimMail.setTo(mailDTO.getEmail());
+    	claimMail.setFrom(FROM_ADDRESS); // 서버에서
+    	claimMail.setTo("youngho.dev@gmail.com"); // 운영자 이메일 주소로 보내기
     	claimMail.setSubject(mailDTO.getName() + " 회원님으로부터 접수된 불편접수 이메일입니다.");
-    	claimMail.setText(mailDTO.getMessage());
+    	claimMail.setText(mailDTO.getMessage() + "\n\n회신 이메일 주소: " + mailDTO.getEmail());
     	
     	mailSender.send(claimMail);
     }
